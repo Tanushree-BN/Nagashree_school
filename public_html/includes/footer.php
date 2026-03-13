26da93ce0b14170b7919f16cd4ce67ff8456b267
Tanushree-BN <tanushreebn2004@gmail.com>
2026-03-13 13:56:31 +0530

    updated header and footer

---
+++ public_html/includes/footer.php
@@ -0,0 +1,55 @@
+<!-- Footer Section -->
+<footer class="ftco-footer ftco-bg-dark ftco-section">
+  <div class="container">
+    <div class="row mb-5">
+      <div class="col-md-6 col-lg-3">
+        <div class="ftco-footer-widget mb-4">
+          <h2 class="ftco-heading-2">Contact Us</h2>
+          <div class="block-23 mb-3">
+            <ul>
+              <li><span class="icon icon-map-marker"></span><span class="text">Shravanabelagola Road, Near Vinayaka Gas Godown, Channarayapatna, Hassan - 573 116</span></li>
+              <li><a href="#"><span class="icon icon-phone"></span><span class="text">Office : +91-9742278222 <br> Principal : +91-9241776070<br> Admin : +91-9901181966</span></a></li>
+              <li><a href="#"><span class="icon icon-envelope"></span><span class="text">nagashreeschoolcrp@gmail.com</span></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-3">
+        <div class="ftco-footer-widget mb-4">
+          <h2 class="ftco-heading-2">Quick Links</h2>
+          <ul class="list-unstyled">
+            <li><a href="index.php"><span class="ion-ios-arrow-round-forward mr-2"></span>Home</a></li>
+            <li><a href="about.php"><span class="ion-ios-arrow-round-forward mr-2"></span>About</a></li>
+            <li><a href="faculties.php"><span class="ion-ios-arrow-round-forward mr-2"></span>Faculties</a></li>
+            <li><a href="facilities.php"><span class="ion-ios-arrow-round-forward mr-2"></span>Facilities</a></li>
+            <li><a href="contact.php"><span class="ion-ios-arrow-round-forward mr-2"></span>Contact</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-3">
+        <div class="ftco-footer-widget mb-4">
+          <h2 class="ftco-heading-2 mb-0">Connect With Us</h2>
+          <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-3">
+            <li class="ftco-animate"><a href="http://www.youtube.com/@Nagashreeenglishschool"><span class="icon-youtube"></span></a></li>
+            <li class="ftco-animate"><a href="https://www.facebook.com/share/17uc3LVVsT/?mibextid=wwXIfr"><span class="icon-facebook"></span></a></li>
+            <li class="ftco-animate"><a href="https://www.instagram.com/nagashree_english_school?igsh=eHhra3JqaTV5eWhq&utm_source=qr"><span class="icon-instagram"></span></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <p>
+          Copyright &copy;<script>document.write(new Date().getFullYear());</script>
+          All rights reserved by
+          <a href="https://nagashreeschoolcrp.in/" target="_blank">Nagashree English School</a>
+        </p>
+        <p>
+          Designed and Developed by
+          <a href="https://mitrasoftwares.in" target="_blank">mitrasoftwares.in</a>
+        </p>
+      </div>
+    </div>
+  </div>
+</footer>
+
---
+++ public_html/includes/header.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Shared site header.
+ *
+ * Usage:
+ *   <?php $activePage = 'home'; include __DIR__ . '/includes/header.php'; ?>
+ */
+if (!isset($activePage)) {
+    $activePage = '';
+}
+
+$nav = [
+    'home' => ['label' => 'Home', 'href' => 'index.php'],
+    'about' => ['label' => 'About', 'href' => 'about.php'],
+    'gallery' => ['label' => 'Gallery', 'href' => 'gallery.php'],
+    'faculties' => ['label' => 'Faculties', 'href' => 'faculties.php'],
+    'facilities' => ['label' => 'Facilities', 'href' => 'facilities.php'],
+    'contact' => ['label' => 'Contact', 'href' => 'contact.php'],
+];
+
+function navItemClass(string $activePage, string $key): string
+{
+    return $activePage === $key ? 'nav-item active' : 'nav-item';
+}
+?>
+
+<!-- Header Section -->
+<div class="bg-top navbar-light">
+  <div class="container">
+    <div class="row no-gutters d-flex align-items-center align-items-stretch">
+      <div class="col-md-4 d-flex align-items-center py-4">
+        <img src="images/nag-logo.png" alt="" class="img-fluid" style="width: 80px; ">
+        <a class="navbar-brand" href="index.php" style="font-size:18px;">Nagashree English School</a>
+      </div>
+      <div class="col-lg-8 d-block">
+        <div class="row d-flex" style="margin-left: 80px;">
+          <div class="col-md d-flex topper align-items-center align-items-stretch py-md-4">
+            <div class="icon d-flex justify-content-center align-items-center"><span class="icon-paper-plane"></span></div>
+            <div class="text">
+              <span>Email</span>
+              <span>nagashreeschoolcrp@gmail.com</span>
+            </div>
+          </div>
+          <div class="col-md d-flex topper align-items-center align-items-stretch py-md-4">
+            <div class="icon d-flex justify-content-center align-items-center"><span class="icon-phone2"></span></div>
+            <div class="text">
+              <span>Call</span>
+              <span style="width: 250px;">Office: +91-9742278222 <br> Principal: +91-9241776070<br> Admin: +91-9901181966</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco-navbar-light" id="ftco-navbar">
+  <div class="container d-flex align-items-center px-4">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="oi oi-menu"></span> Menu
+    </button>
+    <div class="collapse navbar-collapse" id="ftco-nav">
+      <ul class="navbar-nav mr-auto">
+        <?php foreach ($nav as $key => $item): ?>
+          <li class="<?php echo navItemClass($activePage, $key); ?>">
+            <a href="<?php echo htmlspecialchars($item['href'], ENT_QUOTES, 'UTF-8'); ?>" class="nav-link<?php echo $key === 'home' ? ' pl-0' : ''; ?>">
+              <?php echo htmlspecialchars($item['label'], ENT_QUOTES, 'UTF-8'); ?>
+            </a>
+          </li>
+        <?php endforeach; ?>
+      </ul>
+    </div>
+  </div>
+</nav>
+
